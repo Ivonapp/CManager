59204c879799927dfbb558731ab55bd5d04c0eca
Ivonapp <ivona.v.hellsten@hotmail.com>
2025-12-29 15:38:57 +0100

    added code for mock and test for new branch servicetests. Will look through the code further and there might be further changes for this code.

--- CManager.Tests/ServiceTests/CustomerServiceTest.cs
+++ CManager.Tests/ServiceTests/CustomerServiceTest.cs
@@ -1,10 +1,87 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿//SE IGENOM EMILS KOD NEDAN:
 
-namespace CManager.Tests.ServiceTests
+
+
+using CManager.Application.Services;
+using CManager.Domain.Models;
+using CManager.Infrastructure.Repos;
+using Moq;
+
+namespace CManager.Tests.ServiceTests;
+
+public class CustomerServiceTest
 {
-    internal class CustomerServiceTest
+    [Fact]
+    public void DeleteCustomer_WithEmptyGuid_ReturnsFalse()
     {
+        // Arrange
+        var mockCustomerRepo = new Mock<ICustomerRepo>();
+        mockCustomerRepo.Setup(r => r.GetAllCustomers()).Returns(new List<CustomerModel>());
+
+        var service = new CustomerService(mockCustomerRepo.Object);
+
+        // Act
+        var result = service.DeleteCustomer(Guid.Empty);
+
+        // Assert
+        Assert.False(result);
+        mockCustomerRepo.Verify(r => r.GetAllCustomers(), Times.Never);
     }
+
+
+    [Fact]
+    public void DeleteCustomer_WhenCustomerExists_ReturnsTrue()
+    {
+        // Arrange
+        var testCustomer = new CustomerModel
+        {
+            Id = Guid.NewGuid(),
+            FirstName = "test",
+            LastName = "testsson",
+            Email = "test@domain.com",
+            PhoneNumber = "1234567890",
+            Address = new AddressModel
+            {
+                StreetAddress = "Street",
+                City = "City",
+                PostalCode = "12345",
+            }
+        };
+
+        var testCustomers = new List<CustomerModel> { testCustomer };
+
+        var mockCustomerRepo = new Mock<ICustomerRepo>();
+        mockCustomerRepo.Setup(r => r.GetAllCustomers()).Returns(testCustomers);
+        mockCustomerRepo.Setup(r => r.SaveCustomers(It.IsAny<List<CustomerModel>>())).Returns(true);
+
+        var service = new CustomerService(mockCustomerRepo.Object);
+
+        // Act
+        var result = service.DeleteCustomer(testCustomer.Id);
+
+        // Assert
+        Assert.True(result);
+        mockCustomerRepo.Verify(r => r.SaveCustomers(It.IsAny<List<CustomerModel>>()), Times.Once);
+    }
+
+
+
+    [Fact]
+    public void DeleteCustomer_WhenRepositoryReturnsNull_ReturnsFalse()
+    {
+        // Arrange
+        var mockCustomerRepo = new Mock<ICustomerRepo>();
+        mockCustomerRepo.Setup(r => r.GetAllCustomers()).Returns((List<CustomerModel>)null!);
+
+        var service = new CustomerService(mockCustomerRepo.Object);
+
+        // Act
+        var result = service.DeleteCustomer(Guid.NewGuid());
+
+        // Assert
+        Assert.False(result);
+    }
+
+
+
 }
