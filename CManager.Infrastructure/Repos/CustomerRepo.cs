81992145d995692b4aa2bea2c1974cc893de18b4
Ivonapp <ivona.v.hellsten@hotmail.com>
2025-12-31 19:58:46 +0100

    Not finished with this part yet. I have updated the code to allow the user to update customer information, and to ensure that the changes are also saved to the JSON file. Might be updated more and changed more when I have resolved the other sections.

--- CManager.Infrastructure/Repos/CustomerRepo.cs
+++ CManager.Infrastructure/Repos/CustomerRepo.cs
@@ -67,8 +67,61 @@ public class CustomerRepo : ICustomerRepo
         }
         catch (Exception ex)
         {
-            Console.WriteLine($"Error saving file with customers: { ex.Message}");
+            Console.WriteLine($"Error saving file with customers: {ex.Message}");
             return false;
         }
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //                              UPDATECUSTOMER
+    //                              NEDAN KOD ÄR PÅGÅENDE KOD, utgår från indexeringen som Emil skapade i CustomerService.cs > DeleteCustomer
+
+
+    public bool UpdateCustomer(CustomerModel updatedCustomer)
+    {
+        try
+        {
+            
+            var customers = GetAllCustomers(); //Hämtar ALLA kUnderna 
+            
+            var customer = customers.FirstOrDefault(c => c.Id == updatedCustomer.Id); // Hitta kunden med ID, Använde samma indexeringsmetod som i CustomerService.Cs > DeleteCustomer()
+
+            if (customer == null)
+                return false;
+
+                // Uppdaterar och skriver över tidigare sm användaren skrivit in(customer) och skriver över det på (updatedCustomer) MEN uppdaterar ej Json filen än
+                customer.FirstName = updatedCustomer.FirstName;
+                customer.LastName = updatedCustomer.LastName;
+                customer.Email = updatedCustomer.Email;
+                customer.PhoneNumber = updatedCustomer.PhoneNumber;
+                customer.Address.StreetAddress = updatedCustomer.Address.StreetAddress;
+                customer.Address.PostalCode = updatedCustomer.Address.PostalCode;
+                customer.Address.City = updatedCustomer.Address.City;
+
+            
+            return SaveCustomers(customers); // HÄR uppdateras det  till Json
+        }
+
+            catch (Exception ex) //OM det blir fel så hamnar det här och kastar exception
+        {
+            Console.WriteLine($"Not possible to make change. Please try again later... {ex.Message}");
+            return false;
+        }
+    }
+
 }
