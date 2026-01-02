bcb25807150a04d6aea720fde1084cd08fcf6be1
Ivonapp <ivona.v.hellsten@hotmail.com>
2026-01-02 03:18:06 +0100

    Was not able to change the code to my liking. Have went back to the old code and will for now not try to change UpdateCustomer code any further.

--- CManager.Domain/Models/CustomerModel.cs
+++ CManager.Domain/Models/CustomerModel.cs
@@ -10,7 +10,7 @@ namespace CManager.Domain.Models
         public string FirstName { get; set; } = null!; //Null här betyder att denna är required, och att vi GARANTERAR att den kommer fyllas i
         public string LastName { get; set; } = null!;  //Null här betyder att denna är required, och att vi GARANTERAR att den kommer fyllas i
         public string Email { get; set; } = null!;
-        public string? PhoneNumber { get; set; } //Frågetecknet framför string säger att denna är frivillig att fylla i
+        public string PhoneNumber { get; set; } = null!; 
         public AddressModel Address { get; set; } = null!; // Vi lägger in adressen under AdressModell för SOLID principen för "single responsibilities"
 
     }
--- CManager.Presentation.ConsoleApp/Helpers/InputHelper.cs
+++ CManager.Presentation.ConsoleApp/Helpers/InputHelper.cs
@@ -4,16 +4,14 @@ namespace CManager.Presentation.ConsoleApp.Helpers;
 
 public enum ValidationType
 {
-    Required,               //CreateCustomer
-    NotRequired,            //UpdateCustomer
-    Email,                  //CreateCustomer
-    EmailOptional,          //UpdateCustomer
+    Required,
+    Email,
 }
 
 public static class InputHelper
 {
 
-    public static string? ValidateInput(string fieldName, ValidationType validationType)
+    public static string ValidateInput(string fieldName, ValidationType validationType)
     {
         while (true)
         {
@@ -22,15 +20,10 @@ public static class InputHelper
 
             if (string.IsNullOrWhiteSpace(input))
             {
-                if (validationType == ValidationType.NotRequired || validationType == ValidationType.EmailOptional) //UpdateCustomer, kan lämnas tomt
-                    return null;      //UpdateCustomer, kan lämnas tomt - "null" så att inte den gamla informationen suddas ut om användaren ej fyller i nåt
-
-
                 Console.WriteLine($"{fieldName} is required. Press any key to try again....");
                 Console.ReadKey();
                 continue;
             }
-
 
             var (isValid, errorMessage) = ValidateType(input, validationType);
 
@@ -42,12 +35,6 @@ public static class InputHelper
         }
 
     }
-
-
-
-
-
-
 
     private static (bool isValid, string errorMessage) ValidateType(string input, ValidationType type)
     {
@@ -67,34 +54,11 @@ public static class InputHelper
                     return (false, "Invalid email. Use name@example.com ");
                 }
 
-                //CustomerUpdate
-                //Email är valfri men OM kund fyller i email så ska det föhlja regex
-            case ValidationType.EmailOptional:
-                if (string.IsNullOrWhiteSpace(input))
-                { 
-                    return (true, ""); // OM EMAIL LÄMNAS TOM
-                }
-
-                if (IsValidEmail(input))  
-                {
-                    return (true, ""); //OM KUND FYLLER I EMAIL SOM FÖLJER REGEX
-                }
-
-                else
-                {
-                    return (false, "Invalid email. Use name@example.com"); //oM EMAIL FYLLS I MEN INTE FÖLJER REGEX
-                }
-
-
             default:
                 return (true, "");
         }
 
     }
-
-
-
-
 
     private static bool IsValidEmail(string input)
     {
